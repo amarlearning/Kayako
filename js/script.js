3418ded7da107780cfa0ebef60c7a66e45198bfd
Amar Prakash Pandey <amar.om1994@gmail.com>
2017-04-22 02:42:51 +0530

    scroll animation added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.scroll = ScrollReveal({ duration: 1200, scale: 0.9});
+
+scroll.reveal('.p-date', { 	distance: '70px', scale: 0.9 });
+scroll.reveal('.p-description', { distance: '70px', scale: 0.9 });
+scroll.reveal('.button-container', { distance: '70px', scale: 0.9 });
+scroll.reveal('.circular', { distance: '70px', scale: 0.9 });
+scroll.reveal('.info-container p', { distance: '70px', scale: 0.9 });
+scroll.reveal('.two-left', { distance: '100px', origin: 'left', scale: 0.8 });
+scroll.reveal('.two-right', { distance: '20px', origin: 'right', scale: 0.9 });
+scroll.reveal('.video-play-button', { distance: '20px', scale: 0.8 });
+scroll.reveal('.left-up-image', { distance: '50px', origin: 'left', scale: 0.9 });
+scroll.reveal('.right-up-image', { distance: '50px', origin: 'right', scale: 0.9 });
+scroll.reveal('.center-up-image', { distance: '50px', scale: 0.9 });
+scroll.reveal('.xtwo', { distance: '50px', origin: 'right', scale: 0.9 });
+scroll.reveal('.xfour', { distance: '50px', origin: 'right', scale: 0.9 });
+scroll.reveal('.xone', { distance: '50px', scale: 0.9 });
+scroll.reveal('.xthree', { distance: '50px', scale: 0.9 });
+scroll.reveal('.date', { distance: '50px', origin: 'left', scale: 0.9 });
+scroll.reveal('.info', { distance: '50px', origin: 'right', scale: 0.9 });
+scroll.reveal('.faq-content h2', { distance: '50px', origin: 'right', scale: 0.9 });
+scroll.reveal('.journey-image', { distance: '50px', scale: 0.9 });
+scroll.reveal('.wrap-sponsers h2', { distance: '50px', origin: 'left', scale: 0.9 });
